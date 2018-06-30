33abfa20e5e74957559b13c1687a7fe2f48d6334
Yu Jo <zoey5538@gmail.com>
2018-06-30 17:33:32 +0100

    Analyze each tweet seperately

--- BotWork.py
+++ BotWork.py
@@ -9,18 +9,18 @@ api = twitter.Api(consumer_key=config.CONSUMER_KEY,
                   access_token_key=config.ACCESS_TOKEN,
                   access_token_secret=config.ACCESS_TOKEN_SECRET)
 
-
 search=api.GetSearch(term='theresa may', count=100, result_type="recent")
 
 negCount = 0
 
 for s in search:
-    analysisreq = requests.post("http://text-processing.com/api/sentiment/", data=s.text)
+    payload = {'text': s.text}
+    analysisreq = requests.post("http://text-processing.com/api/sentiment/", data=payload)
+    analysis = json.loads(analysisreq.text)
+    label = analysis['label']
+    if (label == "neg"):
+        negCount += 1
 
-analysisreq = requests.post("http://text-processing.com/api/sentiment/", data=payload)
-
-analysis=json.loads(analysisreq.text)
-
-update= "beep boop I'm a bot, theresa may's negativity is at: " + str(analysis['probability']['neg'])
+update= "beep boop I'm a bot, theresa may's negativity is at: " + str(negCount) + "%"
 
 api.PostUpdate(update)
