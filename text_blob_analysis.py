b3f0b68ecd3fc4e132d22a095d340775bbfc3c7c
Yu Jo <zoey5538@gmail.com>
2018-06-30 18:03:02 +0100

    simplify get_approval_ratio

--- text_blob_analysis.py
+++ text_blob_analysis.py
@@ -14,26 +14,16 @@ access_secret = config.ACCESS_TOKEN_SECRET
 auth = OAuthHandler(consumer_key,consumer_secret)
 auth.set_access_token(access_key, access_secret)
 
-api = tweepy.API(auth, wait_on_rate_limit=True)
+api = tweepy.API(auth)
 
 def get_approval_ratio(person, number):
     tweets = api.search(person,'en',count = number)
 
-    positive = 0
-    neutral = 0
-    negative = 0
+    negCount = 0
     for tweet in tweets:
         text = TextBlob(tweet.text)
         polarity = text.sentiment.polarity
-        if polarity>0:
-            positive += 1
-        elif polarity<0:
+        if polarity < 0:
             negative += 1
-        else:
-            neutral += 1
 
-    if negative==0:
-        negative = 1
-    ratio = (positive*100.0)/(positive+negative)
-
-    return [positive, neutral, negative, ratio]
+    return [negCount, len(tweets)]
