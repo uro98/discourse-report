5799497b7619a1f343882123fe8d2fdda3275499
Angus Shaw <shawa950@gmail.com>
2018-06-30 20:53:58 +0100

    Updated to fit with new text_blob_analysis

--- text_blob_test.py
+++ text_blob_test.py
@@ -1,10 +1,13 @@
 from text_blob_analysis import *
 
-politicians = ["Theresa May", "Jeremy Corbyn", "Boris Johnson", "Nicola Sturgeon", "Sadiq Khan", "Philip Hammond", "John Bercow", "Michel Barnier"]
+politicians = ["Theresa May", "Jeremy Corbyn", "Boris Johnson", "Nicola Sturgeon", "Sadiq Khan", "Philip Hammond", "John Bercow", "Michel Barnier", "Arlene Foster"]
+number = 100
+includeRetweets = True
 
 print ""
 for politician in politicians:
-    [positive, neutral, negative, ratio] = get_approval_ratio(politician, 100)
-    print "Person analysed:  ", politician
-    print "Ratio (1=neutral): " + str(round(ratio, 2)) + "%"
+    [positive, neutral, negative, ratio] = get_approval_ratio(politician, number, includeRetweets)
+    print "    Person:", politician
+    print "Popularity: " + str(round(ratio, 2)) + "%"
     print ""
+
